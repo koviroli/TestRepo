3c03cd123029273b2c01141308228b57a597eee7
Kovacs Roland <kovacs.roland.labtech@gmail.com>
2018-06-04 11:01:24 +0200

    new new func2

--- func.cpp
+++ func.cpp
@@ -2,3 +2,12 @@ void func(int* ptr)
 {
 	std::cout << "func(int*)" << std::endl;
 }
+
+void funcNew(int* ptr, int length)
+{
+	ptr[0x00] = 0xA;
+	if(length < 5)
+	{
+		std::cout << "low lenghth" << std::endl;
+	}
+}
